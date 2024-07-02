f29e259a51d4afc6374d3c4c5944e63f84af5b55
roozegare-pashmaloo <167227288+roozegare-pashmaloo@users.noreply.github.com>
2024-07-02 20:47:56 +0330

    Add files via upload

---
+++ javascript/first.js
@@ -0,0 +1,16 @@
+
+let name = "amir";
+let sname = "zehtab";
+let fname = "amir", lname = "zehtab";
+//برای عوض کردن مقدار
+sname = "zehtabchi";
+console.log(name);
+//کلمات کلیدی اسم نمیشن
+//ثابت
+const firstname = "ali";
+//-----------------
+let age = 20;
+
+
+
+//primitive (value) type
